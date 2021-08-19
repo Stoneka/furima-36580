1c3bf409073f61ebdb0ef749cf9dc4e913773c19
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-08-19 16:26:36 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,15 @@ class Item < ApplicationRecord
   belongs_to :burden_method
   belongs_to :shipment_source
   belongs_to :shipping_days
+
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  with_options presence: true, numericality: { other_than: 1 , message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :burden_method
+    validates :shipment_source
+    validates :shipping_days
+  end
+  validates :price, numericality: [{greater_than_or_equal_to: 300}, {less_than_or_equal_to: 9,999,999}]
 end
