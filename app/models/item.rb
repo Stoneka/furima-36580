7bf17325d861d5dfa1a41f2f78dc3907239cb04c
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-08-19 15:44:56 +0900

    アソシエーションの定義(ActiveHash)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden_method
+  belongs_to :shipment_source
+  belongs_to :shipping_days
 end
