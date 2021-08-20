0d291313c502b86426bbed9c1fe8a4fb7c3ca4ff
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-08-20 15:26:42 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,27 +25,27 @@ RSpec.describe Item, type: :model do
         expect(@item).to be_valid
       end
       it 'category_idが空でなければ登録できる' do
-        @item.category_id = '2'
+        @item.category_id = 2
         expect(@item).to be_valid
       end
       it 'status_idが空でなければ登録できる' do
-        @item.status_id = '2'
+        @item.status_id = 2
         expect(@item).to be_valid
       end
       it 'burden_method_idが空でなければ登録できる' do
-        @item.burden_method_id = '2'
+        @item.burden_method_id = 2
         expect(@item).to be_valid
       end
       it 'shipment_source_idが空でなければ登録できる' do
-        @item.shipment_source_id = '2'
+        @item.shipment_source_id = 2
         expect(@item).to be_valid
       end
       it 'shipping_days_idが空でなければ登録できる' do
-        @item.shipping_days_id = '2'
+        @item.shipping_days_id = 2
         expect(@item).to be_valid
       end
       it 'priceが空でないかつ300~9,999,999の間であれば登録できる' do
-        @item.price = '300'
+        @item.price = 300
         expect(@item).to be_valid
       end
     end
@@ -67,27 +67,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
       it 'category_idが初期値では登録できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'status_idが初期値では登録できない' do
-        @item.status_id = '1'
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it 'burden_method_idが初期値では登録できない' do
-        @item.burden_method_id = '1'
+        @item.burden_method_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Burden method can't be blank")
       end
       it 'shipment_source_idが初期値では登録できない' do
-        @item.shipment_source_id = '1'
+        @item.shipment_source_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipment source can't be blank")
       end
       it 'shipping_days_idが初期値では登録できない' do
-        @item.shipping_days_id = '1'
+        @item.shipping_days_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping days can't be blank")
       end
@@ -97,17 +97,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceが300未満の値では登録できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが9,999,999を超える値では登録できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
-      it 'priceが半角数字以外では登録できない' do
+      it 'priceが全角数字では登録できない' do
         @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'priceが半角英数字混合では登録できない' do
+        @item.price = 'ab12'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'priceが半角英字では登録できない' do
+        @item.price = 'abcd'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
