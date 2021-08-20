be28e95e87e87d3423c512fa57b216d96e3170fd
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-08-21 01:09:22 +0900

    ログインの有無による編集ページの表示の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update ]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
