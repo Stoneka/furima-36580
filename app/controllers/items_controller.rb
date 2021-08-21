b866d990102390ed4dfa49b33f46d6caea6f990f
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-08-21 12:31:59 +0900

    商品削除ボタンの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,10 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    #  if current_user.id = @item.user_id
-    #    redirect_to root_path
-    #    end
-    #  @item = Item.find(params[:id])
         @item.destroy
         redirect_to root_path
   end
