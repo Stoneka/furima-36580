9e7f333338e2203e7454b98dd69095e01994b068
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-08-22 13:15:53 +0900

    ordersコントローラー作成、購入画面の配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/index'
   devise_for :users
   root to: "items#index"
   resources :items do
-    resources :order only: [:index, :create]
+    resources :order, only: [:index, :create]
   end
 end
