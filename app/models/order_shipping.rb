b8c6eb0ac59fbbda3cd88c936ff29a05d44a0d2e
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-08-22 14:34:20 +0900

    Order_shippingクラス作成(Formオブジェクト)

---
+++ app/models/order_shipping.rb
@@ -0,0 +1,21 @@
+class OrderShipping
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :shipment_source_id, :city, :block, :building, :phone
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :shipment_source_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :city
+    validates :block
+    validates :pohone, format: {with: /\d{10,11}/}
+  end
+  validates :building, allow_blank: true
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Shipping.create(postal_code: postal_code, shipment_source_id: shipment_source_id,
+       city: city, block: block, building: building, phone: phone, order_id: order.id)
+  end
+end
